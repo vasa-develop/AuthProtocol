63becc820cf83a8262751fa4744c58bd562dd415
vasa-develop <vasa.develop@gmail.com>
2018-02-27 00:14:20 +0530

    made some changrs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HEY!')
+  res.send('HEY VASA!')
 })
 app.listen(4000, () => console.log('Server running on port 4000'))
